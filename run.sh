5bfc44e16786f421cb5c8095b0251b53f5aba140
kaniikura <constantl492@gmail.com>
2020-03-03 11:16:56 +0900

    Update

--- run.sh
+++ run.sh
@@ -32,19 +32,19 @@ data='cifar10'
 #done
 
 # autoencoder
-for model in 'l2_autoencoder' 'ssim_autoencoder'
-    for t in 'plane' 'car' 'bird' 'cat' 'deer' 'dog' 'frog' 'horse' 'ship' 'truck'
-    do
-        echo $t >> result.txt
-        python run_autoencoder.py train with configs/$data/$model.yaml -f dataset.params.anomaly_classes=$t
-        python run_autoencoder.py inference with configs/$data/$model.yaml -f dataset.params.anomaly_classes=$t >> result.txt
-        rm -r train_dirs/$data/ssimae
-        rm -r train_dirs/$data/l2ae
-    done
-done
+#for model in 'l2_autoencoder' 'ssim_autoencoder'
+#    for t in 'plane' 'car' 'bird' 'cat' 'deer' 'dog' 'frog' 'horse' 'ship' 'truck'
+#    do
+#        echo $t >> result.txt
+#        python run_autoencoder.py train with configs/$data/$model.yaml -f dataset.params.anomaly_classes=$t
+#        python run_autoencoder.py inference with configs/$data/$model.yaml -f dataset.params.anomaly_classes=$t >> result.txt
+#        rm -r train_dirs/$data/ssimae
+#        rm -r train_dirs/$data/l2ae
+#    done
+#done
 
 # gan
-"""for model in 'f-anogan'
+for model in 'f-anogan'
     for t in 'plane' 'car' 'bird' 'cat' 'deer' 'dog' 'frog' 'horse' 'ship' 'truck'
     do
         echo $t >> result.txt
@@ -52,4 +52,4 @@ done
         python run_gan.py inference with configs/$data/$model.yaml -f dataset.params.anomaly_classes=$t >> result.txt
         rm -r train_dirs/$data/sagan
     done
-done"""
+done
